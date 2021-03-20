78569873227c97bf7d3ff955ce6d5d27d5f67caa
wromich <pd3391@pjwstk.edu.pl>
2021-03-20 17:21:03 +0100

    wektory

--- pierwszy.R
+++ pierwszy.R
@@ -13,3 +13,21 @@ weatherJSON <- fromJSON(weatherText,flatten=FALSE)
 weatherJSON
 weatherDF <- as.data.frame(weatherJSON)
 weatherDF 
+
+v1<-c(1,2,3,4,5,6,7,8,9,10)
+v1<-as.integer(v1)
+v2<-as.vector(c(2,4,3),mode="integer")
+
+wynik<- v1-v2
+class(wynik)
+wynik<- v1*v2
+class(wynik)
+wynik<- v1+v2
+class(wynik)
+wynik<- v1/v2
+class(wynik)
+
+
+
+wynik<- v1%%v2
+wynik<- v1%/%v2
